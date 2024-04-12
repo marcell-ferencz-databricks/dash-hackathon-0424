2a3f9cea8d81ca30448fa1c46d0d82a1e08005d8
marcell-ferencz-databricks <marcell-ferencz-databricks>
2024-04-12 15:08:46 +0000

    Fix setup_gdal

--- setup_gdal.py
+++ setup_gdal.py
@@ -7,8 +7,6 @@ import mosaic as mos
 
 mos.enable_mosaic(spark, dbutils)
 mos.setup_gdal(
-  to_fuse_dir="/Workspace/Users/marcell.ferencz@databricks.com/DASH Hackathon/gdal",
   with_ubuntugis=False,
-  with_mosaic_pip=True,
-  override_mosaic_version="==0.4.0"
+  with_mosaic_pip=True
 )
